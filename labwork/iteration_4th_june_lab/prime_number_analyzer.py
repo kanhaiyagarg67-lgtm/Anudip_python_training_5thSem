f6703d44129ba5b1520cb57ecb43ecb1a4ee59ac
Kanhaiya Garg <kanhaiyagarg67@gmail.com>
2026-06-04 14:29:02 +0530

    Implement prime number analyzer program
    Added a program to analyze if a number is prime and print its factors.

--- labwork/iteration_4th_june_lab/prime_number_analyzer.py
+++ labwork/iteration_4th_june_lab/prime_number_analyzer.py
@@ -1,1 +1,15 @@
-
+#program for prime number analyzer
+num=int(input("Enter a number: "))
+count=0
+for i in range(1,num+1):
+    if num%i==0:
+        count=count+1
+if count==2:
+    print(num,"is a prime number.") 
+else:
+    print("factors:",end="") #end is used to print all factors in same line
+    for i in range(1,num+1):
+        if num%i==0:
+            print(i,end=" ")
+    print()
+    print(num,"is not a prime number.")
